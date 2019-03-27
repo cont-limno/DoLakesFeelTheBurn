58015110b7b3615c28b292cf60edc8788914692a
immccull95 <imccullough@bren.ucsb.edu>
2019-03-27 11:33:23 -0400

    comparing iws, buffer, lake size

--- RCode/CompareIWS_toBuffers.R
+++ RCode/CompareIWS_toBuffers.R
@@ -1,6 +1,6 @@
-######################## LAGOS US watershed fire severity #######################################
+######################## Comparing buffer, watershed and lake size ##############################
 # Date: 2-1-19
-# updated: 
+# updated: 3-27-19
 # Author: Ian McCullough, immccull@gmail.com
 #################################################################################################
 
@@ -16,37 +16,48 @@ library(rgeos)
 # All LAGOS GIS data in same coordinate system
 #states_shp <- shapefile("C:/Ian_GIS/cb_2016_us_state_500k/lower48.shp")
 #lakes_4ha_pts <- shapefile("C:/Ian_GIS/LAGOS_US_4ha_lakes/LAGOS_US_All_Lakes_4ha_pts/LAGOS_US_All_Lakes_4ha_pts.shp")
-lakes_4ha_all <- shapefile("C:/Ian_GIS/LAGOS_US_4ha_lakes/LAGOS_US_All_Lakes_4ha_v0.2.shp")
+lakes_4ha_all <- shapefile("C:/Ian_GIS/LAGOS-NE-GISv1.0/LAGOS_NE_All_Lakes_4ha/LAGOS_NE_All_Lakes_4ha.shp")
 
 #states_shp <- spTransform(states_shp, CRSobj = crs(lakes_4ha_all))
 
 # read in burned lagoslakeids
-burned_watersheds <- read.csv("ExportedData/Burned1500mBuffs.csv")[,2] #reads 2nd column (lagoslakeid)
+#burned_watersheds <- read.csv("ExportedData/Burned1500mBuffs.csv")[,2] #reads 2nd column (lagoslakeid)
 
 #### define constants ####
-first_year = 1984 #in MTBS dataset
-last_year = 2015
-
-#### load functions ####
-# function to buffer lakes, erasing lake area
-source("RCode/functions/Buffer_erase_lagoslakeid.R")
-# function to tabulate area of different burn severity classes in MTBS dataset for each lake buffer
-source("RCode/functions/tabulate_burn_severity.R")
+#first_year = 1984 #in MTBS dataset
+#last_year = 2015
 
 ########################## Main program ########################
+#lakes_4ha_sub <- subset(lakes_4ha_all, lagoslakei %in% burned_watersheds)
+#Buff1500m_all_burned <- gBuffer(lakes_4ha_sub, byid=T, width=1500)
+#Buff1500m_all_burned <- gBuffer(lakes_4ha_all, byid=T, width=1500)
 Buff1500m_all <- gBuffer(lakes_4ha_all, byid=T, width=1500)
-
-Buff1500m_all_burned <- subset(Buff1500m_all, lagoslakei %in% burned_watersheds)
-Buff1500m_all_burned_area <- gArea(Buff1500m_all_burned, byid=T)
-buff_df <- data.frame(lagoslakeid=Buff1500m_all_burned@data$lagoslakei, buff_ha=(Buff1500m_all_burned_area/10000))
+Buff1500m_all_area <- gArea(Buff1500m_all, byid=T)
+buff_df <- data.frame(lagoslakeid=Buff1500m_all@data$lagoslakei, buff_ha=(Buff1500m_all_area/10000))
 
 # get LAGOS watersheds for NE states
 dt <- lagosne_load(version = '1.087.1') #returns list of data.frame objects
-iws_ha <- data.frame(lagoslakeid=dt$iws$lagoslakeid, iws_ha=dt$iws$iws_ha)
+iws_ha <- data.frame(lagoslakeid=dt$iws$lagoslakeid, iws_ha=dt$iws$iws_ha, lakearea_ha=dt$iws$iws_lakeareaha)
 
+# get master data frame for plotting/correlation analysis
 buff_iws_df <- merge(buff_df, iws_ha, all.x=F, by='lagoslakeid')
 
-# should play around with axis limits so can see data better
-plot(buff_iws_df$buff_ha ~ buff_iws_df$iws_ha, pch=20, xlim=c(0,5000), ylim=c(0,5000),
-     xlab='Watershed area (ha)', ylab='Buffer area (ha)')
-abline(0,1)
+cor(buff_iws_df[,2:4], method='pearson')
+
+#jpeg('C:/Ian_GIS/FeelTheBurn/Paper1/Figures/buff_iws_lake_area.jpeg',width = 6,height = 4,units = 'in',res=300) 
+  par(mfrow=c(1,2))
+  # buffer size vs. iws size
+  plot(buff_iws_df$buff_ha ~ buff_iws_df$iws_ha, pch=20, xlim=c(0,20000), ylim=c(0,20000),
+      xlab='Watershed area (ha)', ylab='Buffer area (ha)', main='LAGOS NE, 51000 lakes')
+  abline(0,1)
+  cor_plot <- round(cor(buff_iws_df$buff_ha, buff_iws_df$iws_ha, method='pearson'),2)
+  legend('topleft', paste0("r = ", cor_plot), bty='n')
+
+  # buffer size vs. lake size
+  plot(buff_iws_df$buff_ha ~ buff_iws_df$lakearea_ha, pch=20, xlim=c(), ylim=c(),
+     xlab='Lake area (ha)', ylab='Buffer area (ha)', main='LAGOS NE, 51000 lakes')
+  abline(0,1)
+  cor_plot2 <- round(cor(buff_iws_df$buff_ha, buff_iws_df$lakearea_ha, method='pearson'),2)
+  legend('topleft', paste0("r = ", cor_plot2), bty='n')
+#dev.off()
+
